cc370244c5ed1c387256b5759e9ee6ecfa6fd3bb
infiniteperplexity <Glenn.T.Wright@kp.org>
2018-03-30 14:25:30 -0700

    added bleaching algorithms

--- modified-voronoi.js
+++ modified-voronoi.js
@@ -49,10 +49,41 @@ Update:
 
 
 var COLOR = "red";
+var BLEACHED = 0;
 var LINEWIDTH = 5;
 var DRAWSITES = false;
 var BACKGROUND = "black";
 var BORDER = "black";
+
+
+function animate() {
+	Voronoi.init();
+	let hues = 256;
+	let steps = 4;
+	let r = hues;
+	let g = 0;
+	let b = 0;
+	let p = 0;
+	setInterval(()=>{
+		BLEACHED+=0.001;
+		if (BLEACHED>1) {
+			BLEACHED = 0;
+		}
+		p = (p+steps)%(hues*3);
+		if (p<hues) {
+			r-=steps;
+			g+=steps;
+		} else if (p<(hues*2)) {
+			g-=steps;
+			b+=steps;
+		} else {
+			b-=steps;
+			r+=steps;
+		}
+		COLOR = "rgb("+(r+(hues-r)*BLEACHED)+","+(g+(hues-g)*BLEACHED)+","+(b+(hues-b)*BLEACHED)+")";
+		Voronoi.draw();
+	},4);
+}
 
 var Voronoi = {
 	//
