a899f986882295b6fcf439e7503b8561f7a47de4
infiniteperplexity <Glenn.T.Wright@kp.org>
2018-04-09 10:58:45 -0700

    took a closer look at time series

--- dataclean.py
+++ dataclean.py
@@ -9,38 +9,19 @@ sst = np.array(infile.variables['sst'][:])
 #lat goes from -89.5 to 89.5
 #lon goes from 0.5 to 359.5
 #time is days since 1891-1-1 (0-46294, 1522 obs)
+#that's wrong, I think...days since 1800, and the lowest is 54?
 #sst is from -5 to 40 (1522, 180, 360)
 #1e20 is missing
 #18° S, 148° E
 
+series = infile.variables["sst"]
+time = infile.variables["time"][:]
+time = [item[0][0] for item in time.tolist()]
+
 series = infile.variables["sst"][:,53:54, 74:75]
 series = [item[0][0] for item in series.tolist()]
 series = [str(s)+"\n" for s in series]
+#pairs = [(time[i], series[i]) for i in range(len(series))]
 outfile = "series.txt"
 with open(path+outfile,'w') as f:
 	f.writelines(series)
-
-
-
-from netCDF4 import Dataset, num2date, date2num
-import matplotlib
-import matplotlib.pyplot as plt
-
-def readnetcdf(rfile,inbounds=False):
-    infile=Dataset(rfile,'r')
-    if np.any(inbounds!=False):
-        outrain=np.array(infile.variables['rainrate'][:,inbounds[3]:inbounds[2]+1,inbounds[0]:inbounds[1]+1])
-        outlatitude=np.array(infile.variables['latitude'][inbounds[3]:inbounds[2]+1])
-        outlongitude=np.array(infile.variables['longitude'][inbounds[0]:inbounds[1]+1])         
-    else:
-        outrain=np.array(infile.variables['rainrate'][:])
-        outlatitude=np.array(infile.variables['latitude'][:])
-        outlongitude=np.array(infile.variables['longitude'][:])
-    outtime=np.array(infile.variables['time'][:],dtype='datetime64[m]')
-    infile.close()
-    return outrain,outtime,outlatitude,outlongitude
-    
-
-
-
-rainfall,time,lat,lon=readnetcdf('ST4.20170828.newregridded.nc')
