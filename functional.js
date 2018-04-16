fa07108c47d21d488a3d7458a77325e5c4fe8227
infiniteperplexity <Glenn.T.Wright@kp.org>
2018-04-16 15:27:41 -0700

    added functional.js

---
+++ functional.js
@@ -0,0 +1,19 @@
+// dhw => stress 0+
+function dhw2stress() {
+}
+
+// bleaching level 0-1, stress 0+ => bleaching level 0-1
+function stress2bleach(stress, bleaching) {
+}
+
+
+
+
+// bleaching level 0-1 => color 0-255
+function bleach2color(range) {
+}
+
+// color 0-255 => visually pleasing LEDs
+function color2leds() {
+
+}
