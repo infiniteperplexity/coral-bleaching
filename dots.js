1a802732f6a7305a5b620c66bd2de37dfba753b9
infiniteperplexity <infinite.perplexity@gmail.com>
2018-04-30 19:21:24 -0700

    put in some tentative logic for bleaching and coral health

--- dots.js
+++ dots.js
@@ -3,14 +3,17 @@ const HEIGHT = 600;
 const DOTS = 512;
 const RADIUS = 5;
 const RATE = 100;
-let COLOR = "red";
+const HUES = 256;
 let canvas;
 
 let dots = [];
 let dot = {
-	bleaching: 0,
+	bleached: 0,
 	health: 1,
-	dead: false
+	dead: false,
+	r: HUES-1,
+	g: 0,
+	b: 0
 }
 function init() {
 	canvas = document.getElementById("canvas");
@@ -27,64 +30,65 @@ function init() {
 	}
 }
 
+function hue(color, bleached) {
+	return (color+(HUES-color)*bleached).toFixed(0);
+}
 function draw() {
 	let ctx = canvas.getContext("2d");
 	ctx.fillStyle = "black";
 	ctx.rect(0,0,WIDTH,HEIGHT);
 	ctx.fill();
-	ctx.fillStyle = COLOR;
 	for (let d of dots) {
 		ctx.beginPath();
 		ctx.arc(d.x,d.y,RADIUS,0,2*Math.PI);
+		ctx.fillStyle = "rgb("+hue(d.r, d.bleached)+","+hue(d.g, d.bleached)+","+hue(d.b, d.bleached)+")";
 		ctx.fill();
 	}
 }
 
-let months = [];
-let averages = [];
-for (let i=0; i<12; i++) {
-	months.push([]);
-}
-for (let i=0; i<series.length; i++) {
-	months[i%12].push(series[i]);
-}
-for (let month of months) {
-	let sum = month.reduce((a,b)=>(a+b));
-	let len = month.length;
-	let avg = sum/len;
-	averages.push(avg);
-}
-let maxavg = averages.reduce((a,b)=>Math.max(a,b));
-dhweeks = [0,0];
-for (let i=0+2; i<series.length; i++) {
-	let month0 = Math.max(0,series[i-2]-maxavg-1);
-	let month1 = Math.max(0,series[i-1]-maxavg-1);
-	let month2 = Math.max(0,series[i-0]-maxavg-1);
-	let dhw = 4*(month0+month1+month2);
-	dhweeks.push(dhw);
-}
-
 // or should this be "probability of bleaching?"
+// if it's a constant thing, then we need a different constant per dot
+// if it's a probabilistic thing, not so much
+// even if it's probabilistic, I still need a fade in / out function
+// so it sounds like partial bleaching *is* potentially a thing
+// which leans towards a "random effects" model
+// oh wait, or fixed effects...but let's not do that for now
+
+// make everything per day
 function dhw2dbleach(dhw) {
 	if (dhw>=8.0) {
 		// severe bleaching
+		return 0.1; // bleach fully in 10 days
 	} else if (dhw>=4.0) {
 		// significant bleaching
+		return 0.04 // bleach fully in 25 days
 	} else if (dhw>=1.0) {
 		// thermal stress
+		return 0.02 // bleach fully in 50 days
 	} else {
 		// recovery?
+		return -0.02 // recover fully in 50 days
 	}
 	return dbleach;
 }
 
-function bleach2dhealth(bleach, health) {
-	if (bleaching===1) {
-		// death
+function bleach2dhealth(bleach) {
+	if (bleaching>=0.75) {
+		// dying
+		return -0.05 // dies in 20 days
 	} else {
 		// slow recovery?
+		return 0.005 // recover fully in 500 days
 	}
 	return dhealth;
+}
+
+function regrowth() {
+	return 0.00025 // recover fully in 5000 days
+}
+
+function bounds(n) {
+	return Math.max(Math.min(n,1),0);
 }
 
 
@@ -93,7 +97,6 @@ function bleach2dhealth(bleach, health) {
 function animate() {
 	let header = document.getElementById("year");
 	init();
-	let hues = 256;
 	let steps = 4;
 	let r = hues;
 	let g = 0;
@@ -101,25 +104,41 @@ function animate() {
 	let p = 0;
 	let period = 0;
 	setInterval(()=>{
-		BLEACHED = (1/11)+(1/55)*series[period];
-		BLEACHED = Math.max(Math.min(BLEACHED,1),0);
-		//BLEACHED = 1;
-		p = (p+steps)%(hues*3);
+		p = (p+steps)%(HUES*3);
 		if (p<hues) {
 			r-=steps;
 			g+=steps;
-		} else if (p<(hues*2)) {
+		} else if (p<(HUES*2)) {
 			g-=steps;
 			b+=steps;
 		} else {
 			b-=steps;
 			r+=steps;
 		}
-		COLOR = "rgb("+(r+(hues-r)*BLEACHED).toFixed(0)+","+(g+(hues-g)*BLEACHED).toFixed(0)+","+(b+(hues-b)*BLEACHED).toFixed(0)+")";
+		for (let d of dots) {
+			d.r = r;
+			d.g = g;
+			d.b = b;
+		} 
 		draw();
 		period = (period+1)%series.length;
+		let gap = (period===0) ? 7 : days[period]-days[period-1]
+		let dhw = dhws[period];
+		for (let d of dots) {
+			if (d.dead===false) {
+				d.bleached = bounds(d.bleached+dhw2dbleach(dhw));
+				d.health = bounds(d.health+bleachd2dhealth(d.bleached));
+				if (d.health===0) {
+					d.dead = true;
+					d.bleached = 1;
+				}
+			} else {
+				d.bleached = 1;
+				d.health = 0;
+			}
+		} 
 		let year = 1800+days[period]/365.25;
-		year = year.toFixed(2);
+		year = year.toFixed(0);
 		header.innerHTML = "Year: " + year;
-	},100);
+	},RATE);
 }
